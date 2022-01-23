cda99617b7d6fc201d6838f69b1329441ac8fd08
William Rocha <william.bueno.rocha@gmail.com>
2022-01-23 13:12:14 -0300

    hook useState

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import React from 'react'
-import { BrowserRouter as Router } from  'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
@@ -8,12 +8,12 @@ import Content from '../components/layout/Content'
 const App = props => {
 
     return (
-                <div className="App">
-                    <Router>
-                        <Menu />
-                        <Content />
-                    </Router>
-                </div>
+        <div className="App">
+            <Router>
+                <Menu />
+                <Content />
+            </Router>
+        </div>
     )
 }
 
